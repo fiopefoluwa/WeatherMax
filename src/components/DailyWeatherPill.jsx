2fca1164ab47f3b955a1264463f6f6bc4bbe54a1
dev-xero <elo48858@gmail.com>
2024-08-20 12:38:58 +0100

    feat: Update daily weather pills

--- src/components/DailyWeatherPill.jsx
+++ src/components/DailyWeatherPill.jsx
@@ -1,29 +1,53 @@
-import Sunrise from '../assets/weather/Sunrise.svg';
-import Sunset from '../assets/weather/Sunset.svg';
-import ChanceOfRain from '../assets/weather/ChanceOfRain.svg';
-import UV from '../assets/weather/UV.svg';
+import {
+    SunriseIcon,
+    SunsetIcon,
+    Uv02Icon,
+    CloudAngledRainIcon,
+} from 'hugeicons-react';
 
 export default function DailyWeatherPill() {
     return (
         <>
-            <div className="p-2 flex flex-col justify-center items-center rounded-lg border-2 border-[#181B2C] bg-[#0F1017]  w-[200px] h-[160px] cursor-pointer flex-shrink-0 transition-all hover:bg-[#31A9ED] hover:border-[#31A9ED] hover:w-[220px] group">
-                <p className="text-xl -translate-x-6">Sunday</p>
-                <section className="translate-y-2 flex flex-col">
-                    <div className="flex gap-2 text-[#949AB5] text-sm">
-                        <img src={Sunrise} alt={Sunrise} size={30} />
-                        <span>Sunrise :</span>
+            <div className="p-2 flex flex-col justify-center items-center rounded-lg border-2 border-[#181B2C] bg-[#0F1017]  w-[200px] h-[160px] cursor-pointer flex-shrink-0 transition-all hover:bg-[#31A9ED] hover:border-[#31A9ED] hover:w-[220px] group select-none">
+                <p className="text-xl text-center">Sunday</p>
+                <section className="pt-2 flex flex-col group-hover:!text-white">
+                    {/* SUNRISE */}
+                    <div className="flex items-center gap-2 text-[#949AB5] text-sm">
+                        <SunriseIcon
+                            size={24}
+                            className="text-[#31A9ED] group-hover:text-white"
+                        />
+                        <span className="group-hover:text-white">
+                            Sunrise:{' '}
+                        </span>
                     </div>
-                    <div className="flex gap-2 text-[#949AB5] text-sm">
-                        <img src={Sunset} alt={Sunset} size={30} />
-                        <span>Sunset :</span>
+                    {/* SUNSET */}
+                    <div className="flex items-center gap-2 text-[#949AB5] text-sm">
+                        <SunsetIcon
+                            size={24}
+                            className="text-[#31A9ED] group-hover:text-white"
+                        />
+                        <span className="group-hover:text-white">Sunset: </span>
                     </div>
-                    <div className="flex gap-2 text-[#949AB5] text-sm">
-                        <img src={UV} alt={UV} size={30} />
-                        <span>UV Index :</span>
+                    {/* UV INDEX */}
+                    <div className="flex items-center gap-2 text-[#949AB5] text-sm">
+                        <Uv02Icon
+                            size={24}
+                            className="text-[#31A9ED] group-hover:text-white"
+                        />
+                        <span className="group-hover:text-white">
+                            UV Index:{' '}
+                        </span>
                     </div>
-                    <div className="flex gap-2 text-[#949AB5] text-sm">
-                        <img src={ChanceOfRain} alt={ChanceOfRain} size={30} />
-                        <span>Chance of rain :</span>
+                    {/* CHANCE OF RAIN */}
+                    <div className="flex items-center gap-2 text-[#949AB5] text-sm">
+                        <CloudAngledRainIcon
+                            size={24}
+                            className="text-[#31A9ED] group-hover:text-white"
+                        />
+                        <span className="group-hover:text-white">
+                            Chance Of Rain:{' '}
+                        </span>
                     </div>
                 </section>
             </div>
