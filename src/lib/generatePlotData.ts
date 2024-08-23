60d10cc2761895e95d021a076bc3c9b5ba92f2f3
dev-xero <elo48858@gmail.com>
2024-08-24 00:08:06 +0100

    fix: Graph weather data from current day

--- src/lib/generatePlotData.ts
+++ src/lib/generatePlotData.ts
@@ -7,11 +7,12 @@ export interface PlotPoint {
 
 export default function generatePlotData(data: number[]) {
     const plotPoints: PlotPoint[] = [];
+    const currDay = new Date().getDay();
 
-    // Build a dataset of days to value
+    // Build a dataset of days to value, starting from current day
     for (let i = 0; i < 7; i++) {
         plotPoints.push({
-            name: extractDayFromNum(i),
+            name: extractDayFromNum((i + currDay - 1) % 7),
             value: data[i],
         });
     }
