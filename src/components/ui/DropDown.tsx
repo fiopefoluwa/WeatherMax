21085a0e192a6b913cfa1faee83358500d4e14a4
dev-xero <elo48858@gmail.com>
2024-08-23 22:04:28 +0100

    feat: Sort cities list

--- src/components/ui/DropDown.tsx
+++ src/components/ui/DropDown.tsx
@@ -3,11 +3,12 @@ import cities from "../../data/cities.json"
 
 export default function DropDown() {
     const [selectedValue, setSelectedValue] = useState("-- select --");
+    const sortedCities = cities.sort((a, b) => a.name.localeCompare(b.name));
     return (
         <>
             <select name="cities-dd" id="cities-dd" value={selectedValue} onChange={e => setSelectedValue(e.target.value)} className="w-full border-2 border-lighter-border bg-card-bg p-4 rounded-lg">
                 {
-                    cities.map((city, id) => (
+                    sortedCities.map((city, id) => (
                         <option key={id} value={city.name} className="select-none">{city.name}</option>
                     ))
                 }
